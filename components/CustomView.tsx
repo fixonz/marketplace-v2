ea16bbb88823dbd2c3a9082426eec917a2a41684
fixonz <109214021+fixonz@users.noreply.github.com>
2024-10-23 10:51:32 +0300

    Update CustomView.tsx

--- components/CustomView.tsx
+++ components/CustomView.tsx
@@ -2,13 +2,27 @@
 import React from 'react';
 import ItemsTab from './collections/TokenCard'; // Adjust the import path as needed
 import ConnectWalletButton from './navbar/Wallet'; // Adjust the import path as needed
+import { useCollections } from '@reservoir0x/reservoir-kit-ui';
+import { Text, Flex, Box } from 'components/primitives';
+import { useMarketplaceChain } from 'hooks';
 
 const CustomView = () => {
+  const marketplaceChain = useMarketplaceChain();
+  const { data: collections } = useCollections({ limit: 3, sortBy: '1DayVolume', includeTopBid: true });
+
   return (
-    <div>
+    <Flex direction="column" align="center" mb={4}>
+      <Text as="h1">Collections</Text>
+      <Flex wrap="wrap" justify="center" mb={4}>
+        {collections?.map((collection) => (
+          <Box key={collection.id} mr={4}>
+            {collection.name}
+          </Box>
+        ))}
+      </Flex>
       <ItemsTab />
       <ConnectWalletButton />
-    </div>
+    </Flex>
   );
 };
 
