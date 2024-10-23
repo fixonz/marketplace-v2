a0bf107794d30946f12952dc2a23d5c3a2597632
fixonz <109214021+fixonz@users.noreply.github.com>
2024-10-23 10:51:08 +0300

    Update index.tsx

--- pages/[chain]/index.tsx
+++ pages/[chain]/index.tsx
@@ -1,293 +1,36 @@
+// src/pages/ages/[chain]/index.tsx
 import {
   GetStaticPaths,
   GetStaticProps,
   InferGetStaticPropsType,
   NextPage,
-} from 'next'
-import { Text, Flex, Box, Button } from 'components/primitives'
-import Layout from 'components/Layout'
-import { ComponentPropsWithoutRef, useContext, useState } from 'react'
-import { Footer } from 'components/home/Footer'
-import { useMediaQuery } from 'react-responsive'
-import { useMarketplaceChain, useMounted } from 'hooks'
-import { paths } from '@reservoir0x/reservoir-sdk'
-import { useCollections } from '@reservoir0x/reservoir-kit-ui'
-import fetcher from 'utils/fetcher'
-import { NORMALIZE_ROYALTIES } from '../_app'
-import supportedChains from 'utils/chains'
-import Link from 'next/link'
-import ChainToggle from 'components/common/ChainToggle'
-import CollectionsTimeDropdown, {
-  CollectionsSortingOption,
-} from 'components/common/CollectionsTimeDropdown'
-import { Head } from 'components/Head'
-import { CollectionRankingsTable } from 'components/rankings/CollectionRankingsTable'
-import { ChainContext } from 'context/ChainContextProvider'
-import { Dropdown, DropdownMenuItem } from 'components/primitives/Dropdown'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useRouter } from 'next/router'
+} from 'next';
+import { Text, Flex, Box } from 'components/primitives';
+import CustomView from '../../../components/CustomView'; // Adjust the import path as needed
 
-type Props = InferGetStaticPropsType<typeof getStaticProps>
-
-const IndexPage: NextPage<Props> = ({ ssr }) => {
-  const isSSR = typeof window === 'undefined'
-  const isMounted = useMounted()
-  const compactToggleNames = useMediaQuery({ query: '(max-width: 800px)' })
-  const [sortByTime, setSortByTime] =
-    useState<CollectionsSortingOption>('1DayVolume')
-  const marketplaceChain = useMarketplaceChain()
-  const router = useRouter()
-
-  let collectionQuery: Parameters<typeof useCollections>['0'] = {
-    limit: 10,
-    sortBy: sortByTime,
-    includeTopBid: true,
-  }
-
-  const { chain, switchCurrentChain } = useContext(ChainContext)
-
-  if (chain.collectionSetId) {
-    collectionQuery.collectionsSetId = chain.collectionSetId
-  } else if (chain.community) {
-    collectionQuery.community = chain.community
-  }
-
-  const { data, isValidating } = useCollections(collectionQuery, {
-    fallbackData: [ssr.collections[marketplaceChain.id]],
-  })
-
-  let collections = data || []
-
-  let volumeKey: ComponentPropsWithoutRef<
-    typeof CollectionRankingsTable
-  >['volumeKey'] = 'allTime'
-
-  switch (sortByTime) {
-    case '1DayVolume':
-      volumeKey = '1day'
-      break
-    case '7DayVolume':
-      volumeKey = '7day'
-      break
-    case '30DayVolume':
-      volumeKey = '30day'
-      break
-  }
-
+const ChainPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ /* props if any */ }) => {
   return (
-    <Layout>
-      <Head />
-      <Box
-        css={{
-          p: 24,
-          height: '100%',
-          '@bp800': {
-            p: '$6',
-          },
-        }}
-      >
-        <Flex
-          direction="column"
-          css={{
-            mx: 'auto',
-            maxWidth: 728,
-            pt: '$5',
-            textAlign: 'center',
-            alignItems: 'flex-start',
-            '@bp600': { alignItems: 'center' },
-          }}
-        >
-          <Flex
-            css={{
-              mb: '$4',
-              gap: '$3',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              maxWidth: '100%',
-              '@bp600': {
-                flexDirection: 'row',
-                alignItems: 'center',
-              },
-            }}
-          >
-            <Text style="h3" css={{ flexShrink: 0 }}>
-              Explore NFTs
-            </Text>{' '}
-            <Flex css={{ gap: '$3', maxWidth: '100%' }}>
-              <Text style="h3" color="subtle">
-                on
-              </Text>
-              <Dropdown
-                contentProps={{
-                  sideOffset: 8,
-                  asChild: true,
-                  style: {
-                    margin: 0,
-                  },
-                }}
-                trigger={
-                  <Flex
-                    css={{
-                      gap: '$3',
-                      alignItems: 'center',
-                      cursor: 'pointer',
-                      minWidth: 0,
-                    }}
-                  >
-                    <Text style="h3" ellipsify>
-                      {' ' + marketplaceChain.name}
-                    </Text>
-                    <FontAwesomeIcon
-                      icon={faChevronDown}
-                      width={16}
-                      height={16}
-                      color="#9BA1A6"
-                    />
-                  </Flex>
-                }
-              >
-                <Flex direction="column" css={{ minWidth: 150 }}>
-                  {supportedChains.map(({ name, id, routePrefix }) => (
-                    <DropdownMenuItem
-                      css={{
-                        textAlign: 'left',
-                      }}
-                      key={id}
-                      onClick={() => {
-                        const newUrl = router.asPath.replace(
-                          chain.routePrefix,
-                          routePrefix
-                        )
-                        switchCurrentChain(id)
-                        router.replace(newUrl, undefined, { scroll: false })
-                      }}
-                    >
-                      <Text
-                        style="h6"
-                        color={
-                          id === marketplaceChain.id ? undefined : 'subtle'
-                        }
-                        css={{ cursor: 'pointer' }}
-                      >
-                        {name}
-                      </Text>
-                    </DropdownMenuItem>
-                  ))}
-                </Flex>
-              </Dropdown>
-            </Flex>
-          </Flex>
-          <Text style="body1" color="subtle" css={{ mb: 48 }}>
-            Multi-Chain Marketplace powered by OKINA Community
-          </Text>
-        </Flex>
-        <Flex css={{ my: '$6', gap: 65 }} direction="column">
-          <Flex
-            justify="between"
-            align="start"
-            css={{
-              flexDirection: 'column',
-              gap: 24,
-              '@bp800': {
-                alignItems: 'center',
-                flexDirection: 'row',
-              },
-            }}
-          >
-            <Text style="h4" as="h4">
-              OKINA LABS Collections
-            </Text>
-            <Flex align="center" css={{ gap: '$4' }}>
-              <CollectionsTimeDropdown
-                compact={compactToggleNames && isMounted}
-                option={sortByTime}
-                onOptionSelected={(option) => {
-                  setSortByTime(option)
-                }}
-              />
-              <ChainToggle />
-            </Flex>
-          </Flex>
-          {isSSR || !isMounted ? null : (
-            <CollectionRankingsTable
-              collections={collections}
-              loading={isValidating}
-              volumeKey={volumeKey}
-            />
-          )}
-          <Box css={{ alignSelf: 'center' }}>
-            <Link href={`/${marketplaceChain.routePrefix}/collection-rankings`}>
-              <Button
-                css={{
-                  minWidth: 224,
-                  justifyContent: 'center',
-                }}
-                size="large"
-              >
-                View All
-              </Button>
-            </Link>
-          </Box>
-        </Flex>
-        <Footer />
-      </Box>
-    </Layout>
-  )
-}
+    <div>
+      <CustomView />
+      {/* Other components can be added here if needed */}
+    </div>
+  );
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking',
-  }
-}
+  };
+};
 
-type CollectionSchema =
-  paths['/collections/v5']['get']['responses']['200']['schema']
-type ChainCollections = Record<string, CollectionSchema>
-
-export const getStaticProps: GetStaticProps<{
-  ssr: {
-    collections: ChainCollections
-  }
-}> = async () => {
-  let collectionQuery: paths['/collections/v5']['get']['parameters']['query'] =
-    {
-      sortBy: '1DayVolume',
-      normalizeRoyalties: NORMALIZE_ROYALTIES,
-      includeTopBid: true,
-      limit: 10,
-    }
-
-  const promises: ReturnType<typeof fetcher>[] = []
-  supportedChains.forEach((chain) => {
-    const query = { ...collectionQuery }
-    if (chain.collectionSetId) {
-      query.collectionsSetId = chain.collectionSetId
-    } else if (chain.community) {
-      query.community = chain.community
-    }
-    promises.push(
-      fetcher(`${chain.reservoirBaseUrl}/collections/v5`, query, {
-        headers: {
-          'x-api-key': chain.apiKey || '',
-        },
-      })
-    )
-  })
-  const responses = await Promise.allSettled(promises)
-  const collections: ChainCollections = {}
-  responses.forEach((response, i) => {
-    if (response.status === 'fulfilled') {
-      collections[supportedChains[i].id] = response.value.data
-    }
-  })
-
+export const getStaticProps: GetStaticProps = async (context) => {
+  // Fetch any necessary data for the page if needed
   return {
-    props: { ssr: { collections } },
-    revalidate: 5,
-  }
-}
+    props: {
+      // Your props here
+    },
+  };
+};
 
-export default IndexPage
+export default ChainPage;
